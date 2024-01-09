1b49858c8860599fe343cb2dffeee33d07710ed9
karturbis <vulkanera42@gmx.ch>
2024-01-09 14:06:08 +0100

    add main method and pygame.init()

---
+++ main.py
@@ -0,0 +1,24 @@
+import pygame
+
+pygame.init() # Initialize pygame
+
+# Global Variables:
+gravitaccel = 9.81
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    """The following code gets executed, if this file is executed"""
+    
