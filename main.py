71683dca83ef2d47c0e7182d0bd996d9e9dc8ff3
karturbis <vulkanera42@gmx.ch>
2024-01-09 15:21:22 +0100

    add variables for gravitational accelerations of various planets and moons of our solar system

--- main.py
+++ main.py
@@ -3,9 +3,31 @@ import pygame
 pygame.init() # Initialize pygame
 
 # Global Variables:
+merkuraccel = 3.70 # m/s²
+
+venusaccel = 8.87 # m/s²
+
 earthaccel = 9.81 # m/s²
 moonaccel = 1.62 # m/s²
 
+marsaccel = 3.73 # m/s²
+phobosaccel = 0.006 # m/s²
+deimosaccel = 0.003 # m/s²
+
+jupiteraccel = 24.97 # m/s²
+ioaccel = 1.80 # m/s²
+ganymedeaccel = 1.43 # m/s²
+europaaccel = 1.31 # m/s²
+callistoaccel = 1.24 # m/s²
+
+saturnaccel = 10.44 # m/s²
+titanaccel = 1.35 # m/s²
+rheaaccel = 0.26 # m/s²
+
+
+uranusaccel = 8.87 # m/s²
+
+neptunaccel = 11.15 # m/s²
 
 
 
