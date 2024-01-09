ac603a161e489004733c87d9b4226ffeadd32d99
karturbis <vulkanera42@gmx.ch>
2024-01-09 16:14:00 +0100

    add gameloop

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+
+
 import data # Imports the data from the data.py file, this includes the gravitational vlaues.
 
 pygame.init() # Initialize pygame
@@ -7,6 +9,14 @@ pygame.init() # Initialize pygame
 screen = pygame.display.set_mode(data.windowsize)
 
 
+def gameloop():
+    
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == QUIT:
+                pygame.quit()
+                exit(0)
 
 
 
