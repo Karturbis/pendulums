75f5eab19b0091e035d80aaa7e09192414ce9527
karturbis <vulkanera42@gmx.ch>
2024-01-10 12:22:52 +0100

    add comments

--- main.py
+++ main.py
@@ -3,13 +3,24 @@ from pygame.locals import *
 
 import data # Imports the data from the data.py file, this includes the gravitational vlaues and the window size.
 
-pygame.init() # Initialize pygame
+ # Initialize pygame
 
 
-
+def game_init():
+    """this method is ran at the begin of the game
+    it initializes pygame,
+    defines the clock variable,
+    sets the screen size,
+    and sets the window caption."""
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(data.windowsize)
+    pygame.display.set_caption("Pendulums")
 
 def gameloop():
-    
+    """This method contains the game loop, which runs until the game is stopped,
+    it contains all the calls to functions and methods, that have to be called
+    every frame of the game."""
     
     while True: # This loop runs the game until it is stopped
 
@@ -25,11 +36,6 @@ def gameloop():
 
 if __name__ == "__main__":
     """The following code gets executed, if this file is executed"""
-
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(data.windowsize)
-    pygame.display.set_caption("Pendulums")
-
-
+    game_init()
     gameloop()
     
