d2a38a9203a9348fe9f98ee1d4429f6bf9431b2e
karturbis <vulkanera42@gmx.ch>
2024-01-12 10:19:36 +0100

    initialize the pygame in the main function instead of the initialize function, to achief a working init

--- main.py
+++ main.py
@@ -3,19 +3,6 @@ from pygame.locals import *
 
 import data # Imports the data from the data.py file, this includes the gravitational vlaues and the window size.
 
- # Initialize pygame
-
-
-def game_init():
-    """this method is ran at the begin of the game
-    it initializes pygame,
-    defines the clock variable,
-    sets the screen size,
-    and sets the window caption."""
-    pygame.init()
-    clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(data.windowsize)
-    pygame.display.set_caption("Pendulums")
 
 def gameloop():
     """This method contains the game loop, which runs until the game is stopped,
@@ -36,6 +23,18 @@ def gameloop():
 
 if __name__ == "__main__":
     """The following code gets executed, if this file is executed"""
-    game_init()
+
+    """this block is ran at the begin of the game
+    it initializes pygame,
+    defines the clock variable,
+    sets the screen size,
+    and sets the window caption."""
+    pygame.init()
+    clock = pygame.time.Clock()
+    screen = pygame.display.set_mode(data.windowsize)
+    pygame.display.set_caption("Pendulums")
+    
+
+
     gameloop()
     
