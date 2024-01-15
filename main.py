c6cb90b9bb4aa2cce98aefac18c26cee4977c96f
karturbis <vulkanera42@gmx.ch>
2024-01-15 20:57:38 +0100

    remove the data.py file and move the variables into the main.py file

--- main.py
+++ main.py
@@ -3,8 +3,45 @@ import random
 import math
 from pygame.locals import *
 
-import data # Imports the data from the data.py file, this includes the gravitational vlaues and the window size.
+# Gravitational Accelerations:
+gravityaccel = {
+    "merkur": 3.70, 
+    "venus": 8.87, 
+    "earth": 9.81, 
+    "moon": 1.62, 
+    "mars": 3.73, 
+    "phobos": 0.006, 
+    "deimos": 0.003, 
+    "jupiter": 24.97, 
+    "io": 1.80, 
+    "ganymede": 1.43, 
+    "europa": 1.31, 
+    "callisto": 1.24, 
+    "saturn": 10.44, 
+    "titan": 1.35, 
+    "rhea": 0.26, 
+    "uranus": 8.87, 
+    "titania": 0.37,
+    "oberon": 0.35,
+    "ariel": 0.25,
+    "umbriel": 0.25,
+    "neptune": 11.15,
+    "triton": 0.78,
+    "proteus": 0.07,
+    "nereid": 0.07
+    }
 
+# Windowsize:
+windowsize = (1900, 860)
+
+# Position of the pendulum(cord) fixpoint:
+x_fixpoint_position = 200
+y_fixpoint_position = 200
+
+# Position of the target:
+x_target_min = 100
+x_target_max = 200
+y_target = 300
 class Pendulum:
     
     # Position of the loose end of the pendulum:
@@ -22,15 +59,15 @@ class PeendulumWeight(Pendulum):
 class PendulumCord(Pendulum):
 
     def __init__(self, angle, length):
-        self.x_fixpoint_position = data.x_fixpoint_position
-        self.y_fixpoint_position = data.y_fixpoint_position
+        self.x_fixpoint_position = x_fixpoint_position
+        self.y_fixpoint_position = y_fixpoint_position
 
 
 class Target:
 
     def __init__(self):
-        self.x_position = random.randint(data.x_target_min, data.x_target_max)
-        self.y_position = data.y_target
+        self.x_position = random.randint(x_target_min, x_target_max)
+        self.y_position = y_target
 
 
 def gameloop():
@@ -62,7 +99,7 @@ if __name__ == "__main__":
     and sets the window caption."""
     pygame.init()
     clock = pygame.time.Clock()
-    screen = pygame.display.set_mode(data.windowsize)
+    screen = pygame.display.set_mode(windowsize)
     pygame.display.set_caption("Pendulums")
     
 
