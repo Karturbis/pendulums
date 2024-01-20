a08dbeaf231f23f01809f6452df16a821d2f1222
karturbis <vulkanera42@gmx.ch>
2024-01-20 15:02:32 +0100

    add gravitational acceleration values

--- main.py
+++ main.py
@@ -2,6 +2,33 @@ import pygame
 import random
 from pygame.locals import *
 
+# Gravitational Accelerations:
+gravityaccel = {
+    "merkur": 3.70, 
+    "venus": 8.87, 
+    "earth": 9.81, 
+    "moon": 1.62, 
+    "mars": 3.73, 
+    "phobos": 0.006, 
+    "deimos": 0.003, 
+    "jupiter": 24.97, 
+    "io": 1.80, 
+    "ganymede": 1.43, 
+    "europa": 1.31, 
+    "callisto": 1.24, 
+    "saturn": 10.44, 
+    "titan": 1.35, 
+    "rhea": 0.26, 
+    "uranus": 8.87, 
+    "titania": 0.37,
+    "oberon": 0.35,
+    "ariel": 0.25,
+    "umbriel": 0.25,
+    "neptune": 11.15,
+    "triton": 0.78,
+    "proteus": 0.07,
+    "nereid": 0.07
+    }
 
 class Target:
 
